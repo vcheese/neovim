25933250cc9aec1b260aadf935919d068146479e
vcheese <konradsawicki98@gmail.com>
2024-11-17 14:06:47 +0100

    Change key mappings.

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -7,9 +7,9 @@ return {
       
     config = function()
 	    local builtin = require('telescope.builtin')
-	    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+	    vim.keymap.set('n', '<leader>if', builtin.find_files, { desc = 'Telescope find files' })
 	    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
-	    vim.keymap.set('n', '<leader>ps', function()
+	    vim.keymap.set('n', '<leader>is', function()
 		    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 	    end)
     end
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+vim.keymap.set("n", "<leader>ii", vim.cmd.Ex)
+vim.keymap.set("n","w", "b") 
+vim.keymap.set("n", "+", "$")
