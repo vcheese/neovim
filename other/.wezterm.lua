7a4acc1b53ef3e6dacc0260512515c90fed897f8
vcheese <konradsawicki98@gmail.com>
2024-11-17 17:40:47 +0100

    add other cfg files.

---
+++ other/.wezterm.lua
@@ -0,0 +1,45 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+
+config.keys = {
+  {
+    key = 'd',
+    mods = 'CTRL',
+    action = wezterm.action.SendKey {
+      key = 'Backspace',
+    },
+  },
+  {
+    key = 'Tab',
+    action = wezterm.action.SendKey {
+      key = 'Enter',
+    },
+  },
+  {
+    key = '§',
+    action = wezterm.action.SendKey {
+      key = 'Tab',
+    },
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
+
+
+
+
+
+
+
+
+
+
